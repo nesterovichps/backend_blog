a863059ddd77afdf488599ca42507583aeda8785
petr <petr.n.92@list.ru>
2020-11-28 04:07:38 +0600

    layout process

--- main_blog/models.py
+++ main_blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Blog_post(models.Model):
+
+    author=models.CharField('Автор', max_length=50) #TODO fix
+    text_post=models.TextField('Текст статьи') #TODO fix
+    data_post=models.DateTimeField('Дата', max_length=10) #TODO fix
+    # status_write=models.BooleanField('Прочитана\не прочитана', max_length=50) #TODO fix
+
+    def __str__(self):
+        return (self.text_post)
+    class Meta:
+        verbose_name='Пост'
+        verbose_name_plural = 'Посты'
+# class Subscribe(models.Model);
+#     pass
--- main_blog/views.py
+++ main_blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Blog_post
 from . import templates
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main_blog/index.html')
+    post_list=Blog_post.objects.all()
+    return render(request, 'main_blog/index.html', {'post_list': post_list})
 
 
 def auth(request):
